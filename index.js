90873debc839a3b3e9efbd8e212a26c9421ad7b9
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-09-13 19:24:57 -0500

    Initialize progress bar app

---
+++ index.js
@@ -0,0 +1,11 @@
+const slider = document.getElementById('slider');
+const progress = document.getElementById('progress');
+const progressBar = document.getElementById('progress-bar');
+slider.addEventListener('change', (e) => {
+    updateProgress(e.target.value);
+})
+function updateProgress(value) {
+    progress.textContent = value;
+    progressBar.style.width = `${value}%`;
+    progressBar.setAttribute('aria-valuenow', value);
+}
