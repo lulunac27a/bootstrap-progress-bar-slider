ef0b78f0b2ea53cca2f35ca30759eda863e0a1e0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-09-13 19:29:46 -0500

    Add .prettierrc and format code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const slider = document.getElementById("slider");
 const progress = document.getElementById("progress");
 const progressBar = document.getElementById("progress-bar");
 slider.addEventListener("change", (e) => {
-  updateProgress(e.target.value);
+    updateProgress(e.target.value);
 });
 function updateProgress(value) {
-  progress.textContent = value;
-  progressBar.style.width = `${value}%`;
-  progressBar.setAttribute("aria-valuenow", value);
+    progress.textContent = value;
+    progressBar.style.width = `${value}%`;
+    progressBar.setAttribute("aria-valuenow", value);
 }
